be17ce27e3894278f4beae7385a1648d111d93b4
Abdeellatif boutahar <abdeellatifboutahar18@gmail.com>
2023-05-29 13:31:58 +0100

    modif1

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -10,7 +10,6 @@ namespace PartsUnlimited.Models
         [Key]
         //Ma première modification  
         public int CartItemId { get; set; }
-        //Mon deuxieme changement 
         [Required]
         public string CartId { get; set; }
         public int ProductId { get; set; }
